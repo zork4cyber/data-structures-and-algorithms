3b41c7a75d7865582455672e2de0ee9a12dc62b1
Alexander White <parzivalwhite@gmail.com>
2019-04-22 19:47:45 -0700

    testsgit add insertion-sort.js __tests__/insertion-sort.test.js

--- code-challenges/sorts/insertion-sort.js
+++ code-challenges/sorts/insertion-sort.js
@@ -2,20 +2,15 @@
 
 // Write a function for insertion sort that takes in an unsorted array and returns the array sorted using insertion sort.
 
-function insertionSort(arr) {
-  for(let i = 0, j; i < arr.arrlength; i++){
+const insertionSort = (arr) => {
+  for(let i = 1, j; i < arr.length; i++) {
     let temp = arr[i];
-    for(let j = i -1; j >= 0 && arr[j] > temp; j--) {
+    for(j = i - 1; j >= 0 && arr[j] > temp; j--) {
       arr[j+1] = arr[j];
     }
     arr[j+1] = temp;
   }
   return arr;
-}
-
-let oneArr = [1];
-const list = [54, 26, 93, 17, 77, 31, 44, 55, 20];
-console.log(insertionSort(list));
-console.log(insertionSort(oneArr));
+};
 
 module.exports = insertionSort;
